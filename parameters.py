d783cd414c72925606b541f66c3ea581ac7f7fc3
yashivijayvargiya <yashi.vijay24@gmail.com>
2024-06-03 23:58:11 +0530

    Revert "main file"
    This reverts commit 2d17f0c1ea20ca10b77cb0ca943f3ce3ce67bc94.

--- parameters.py
+++
@@ -1,6 +0,0 @@
-#credentials
-
-username = 'yashi.vijay24@gmail.com'
-password = '@anshi0724'
-
-message = "Came across your profile and realized your interest in sports and supporting young talent"
